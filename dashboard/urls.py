25b156a7133e88c0e990b9ebfa9b0526d571bcf9
fredrickmugish <fredrickgotfried@gmail.com>
2023-12-30 20:31:08 +0000

    another commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
 
     path('', views.home),
-    path('notes/',views.notes)
+    path('notes/',views.notes),
+    path('notes/add',views.addNotes)
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,15 @@ def notes(request):
     notes = Notes.objects.filter(user=request.user)
     context = {'notes':notes}
     return render(request, 'dashboard/notes.html', context)
+
+def addNotes(request):
+    if request.method=="POST":
+        t=request.POST['title']
+        d=request.POST['description']
+        user=request.user
+        notes = Notes()
+        notes.title=t
+        notes.description=d
+        notes.user=user
+        notes.save()
+        return HttpResponseRedirect('/notes')
